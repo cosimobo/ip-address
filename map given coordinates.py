b277b4f72ba2c5f6f701dffa1bdbb83028a5d55c
AlbertoScuccato <alberto.scuccato@student.h-farm.com>
2020-12-03 01:56:05 +0100

    prova 1

--- map given coordinates.py
+++ map given coordinates.py
@@ -5,3 +5,17 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("https://freegeoip.app/json")
 
 df.head()
+
+BBox = ((df.longitude.min(),   df.longitude.max(),
+         df.latitude.min(), df.latitude.max()))
+
+# opestreetmap.org https://medium.com/@abuqassim115/thanks-for-your-response-frank-fb869824ede2
+
+ruh_m = plt.imread('C:/.. … /Riyadh_map.png')
+
+fig, ax = plt.subplots(figsize = (8,7))
+ax.scatter(df.longitude, df.latitude, zorder=1, alpha= 0.2, c='b', s=10)
+ax.set_title('Plotting Spatial Data on Riyadh Map')
+ax.set_xlim(BBox[0],BBox[1])
+ax.set_ylim(BBox[2],BBox[3])
+ax.imshow(ruh_m, zorder=0, extent = BBox, aspect= 'equal')
