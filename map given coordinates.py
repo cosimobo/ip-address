8771b0f77dfd9e8e52b232b2bcb2068c2490288a
AlbertoScuccato <alberto.scuccato@student.h-farm.com>
2020-12-03 11:01:46 +0100

    3/12

--- map given coordinates.py
+++ map given coordinates.py
@@ -14,7 +14,16 @@ BBox = ((df.longitude.min(),   df.longitude.max(),
 print(BBox)
 # (-122.9638, 151.1021, -37.6363, 55.7028)
 
+mymap = plt.imread("map.png")
 
+#plot df.longitude and df.latitude coordinates as scatter points on the mymap map image
+
+fig, ax = plt.subplots(figsize = (8,7))
+ax.scatter(df.longitude, df.latitude, zorder=1, alpha= 0.2, c='b', s=10)
+ax.set_title('Plotting Spatial Data on Riyadh Map')
+ax.set_xlim(BBox[0],BBox[1])
+ax.set_ylim(BBox[2],BBox[3])
+ax.imshow(mymap, zorder=0, extent = BBox, aspect= 'equal')
 
 
 
