1a2551ce6a285f6819f5bf8697216db503b39fb1
lorenzocardone <74044857+lorenzocardone@users.noreply.github.com>
2020-12-04 16:01:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
-import ip_function
-import csv_manager
-import databasemanager
-import argparse
+import requests
+import json
 
-ip_address = "1.2.3.4"
-csv_path = "data/ip_data.csv"
-database_path = 'data/databasemanager.db'
 
-databasemanager.db_creation(database_path)
+IP_URL ='http://ip-api.com/json/{}'
 
-def parse_arguments():
-    parser=argparse.ArgumentParser(
-            description="IP ADDRESS prject")
-    parser.add_argument ("ip_address", type=str,
-                         help= "Insert an IP Address",
-                         default=None)
-    parser.add_argument('-u', help="username name (requires -p)",
-                         default=None)
-    parser.add_argument('-p', help="username password",
-                         default=None)
+def get_location(ip_address):
 
-    group=parser.add_mutually_exclusive_group()
-    group.add_argument("-q", "--quiet", action="store_true", help ="print quiet")
-    group.add_argument("-v", "--verbose", action="store_true", help="print verbose")
-    args = parser.parse_args()
+    URL = IP_URL.format(ip_address)
+    r = requests.get(URL)
+    info = json.loads(r.text)
+    #print(info)
+    try:
+        location = info['country']
+        city = info['city']
+    except KeyError:
+        print('Error')
 
-    return args
+    return location, city
 
-if __name__ == "__main__":
-    args = parse_arguments()
-    if databasemanager.check(args.u,args.p):
-        city, country = ip_function.get_location(args.ip_address)
-        info = str("The IP ADDRESS "+ args.ip_address + " is located in "
-                + city + " (" +  country +") ")
-        print (info)
-        #PARTE DI SCUCCATO
-        csv_manager.write_data(csv_path,ip_address,city,country)
+from ip_address import get_location
+
+ip_address = '153.138.24.18'
+
+country, city = get_location(ip_address)
+
+print("IP address {} is from {}, {} ".format(ip_address, city,  country))
